5f76403cfdfb9e03fed5fb336423bc87316d4e6a
dmatchey-eng <dmatchey@gmail.com>
2026-06-18 12:28:00 -0500

    Add PodcastHostCommentator to module exports

--- engine/__init__.py
+++ engine/__init__.py
@@ -6,5 +6,6 @@ Explicitly exposes package components to the runtime namespace.
 from .core_states import MarkovCoreStates
 from .provenance import ProvenanceGate
 from .demonstration import DemonstrationSpace
+from .podcast_host import PodcastHostCommentator
 
-__all__ = ["MarkovCoreStates", "ProvenanceGate", "DemonstrationSpace"]
+__all__ = ["MarkovCoreStates", "ProvenanceGate", "DemonstrationSpace", "PodcastHostCommentator"]
